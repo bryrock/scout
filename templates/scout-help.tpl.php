aa2f9f70ace8b9bc8cdcc76394d95556ba085c05
bryrock <bryan@ideaden.com>
2013-12-10 17:49:55 -0600

    Tidy up Scout help.

--- templates/scout-help.tpl.php
+++ templates/scout-help.tpl.php
@@ -12,7 +12,6 @@
       <ol>
         <li>Install Joyride jQuery plugin.</li>
         <li>Create Scout Tips and Scout Trails.</li>
-        <li>Create Joyride Tips content.</li>
       </ol>
       <hr>
     </div>
@@ -38,35 +37,10 @@
 
   <div class="row">
     <div class="twelve columns">
-      <h2 style="font-size: 14px;">2. Create context to manage your tour.</h2>
+      <h2 style="font-size: 14px;">2. Create Scout Tips and Scout Trails.</h2>
       <br />
       <ol>
-        <li>Navigate to context <a href="/admin/structure/context" target="_blank">overview</a> page.</li>
-        <li>Optionally review module's bundled contexts to get an idea of how they works. Bundler contexts are tagged with "joyride" tag.</li>
-        <li>Create new context as you usually do. In "Conditions" section add "Path" condition and enter URLs of pages where you want to have your Joyride tour enabled.</li>
-        <li>In "Reactions" section paste tips content that will be shown as Joyride tour tips. <a href="#create-tips-content">More info on this.</a></li>
-      </ol>
-      <hr>
-    </div>
-  </div>
-
-  <br />
-
-  <div class="row">
-    <div class="twelve columns">
-      <h2 id="create-tips-content" style="font-size: 14px;">3. Create Joyride Tips content.</h2>
-      <br />
-      <ol>
-        <li>Joyride module expects that tips content will be formatted to use <code>&lt;li&gt;</code> as item wrappers</li>
-        <li>You can use following code as a reference:
-          <pre>
-  &lt;li data-id="step-welcome" data-button="Next" data-options="tipAnimation:fade"&gt;
-    &lt;h5>Tip heading&lt;/h5&gt;
-    &lt;p>Pellentesque ornare sem lacinia quam venenatis vestibulum.&lt;/p&gt;
-  &lt;/li&gt;
-          </pre>
-        </li>
-        <li>Please check <a href="http://www.zurb.com/playground/jquery-joyride-feature-tour-plugin" target="_blank">documentation</a> to see complete list of options and features.</li>
+        <li>See <a href="/admin/help/scout_trails" target="_blank">Scout Trails help index</a> to get started.</li>
       </ol>
       <hr>
     </div>
