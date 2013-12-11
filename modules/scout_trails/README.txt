ccb0a73162a605067dbc830a492adea39c938d5d
bryrock <bryan@ideaden.com>
2013-12-10 20:00:04 -0600

    Scout Trails help and readme.

--- modules/scout_trails/README.txt
+++ modules/scout_trails/README.txt
@@ -1,3 +1,25 @@
 This is the Read Me file for Scout Trails.
 
-<!-- TODO - Complete this. -->
+Scout is a Site Navigation aid. It can be used to assist new visitors to your site. It can also by used to help train content managers, guiding them through steps for creation and administration of site content.
+
+Scout is a module/features package of the Zurb Joyride jQuery library for Drupal 7. The Zurb Joyride jQuery library is currently being implemented in Drupal 8 core as "Tours."
+
+Scout brings Joyride functionality along with the Drupal 8 ease of tour creation to Drupal 7. Scout adds a custom Context reaction plug-in to Drupal 7, but works differently than the plugin provided by the Joyride module. Rather than providing a long text field for entering HTML, Scout's context reaction allows you to select a previously assembled Scout Trail (tour) from a simple pull-down list.
+
+Scout Trails are collections of Scout Tips. Both are custom entities.
+
+Scout Tips are created by filling out a form, in a familiar manner similar to adding Drupal content. HTML is optional, but not required.
+
+Scout Trails are also created via a short form, used for assembling collections of Scout Tip entity references.
+
+Once Scout Trails are created and saved, they are available for use by Context.
+
+Scout Tips and Trails are exportable (using Features), so they can be created once and re-used on multiple sites of similar design.
+
+---
+
+Requires: Context, Chaos tools, Entity API, Entity Connect, Entity Reference, Features, Fieldgroup, Field, Field SQL storage, Inline Editing (will be ditched soon as this is a custom module), Options, List, Number, Scout (included), Libraries, Text, Views
+
+Once installed, there is help information and a skinny tutorial at /admin/help/scout_trails.
+
+<!-- TODO - Remove Inline Editing dependency. -->
--- modules/scout_trails/templates/scout_trails-help.tpl.php
+++ modules/scout_trails/templates/scout_trails-help.tpl.php
@@ -1,8 +1,8 @@
 <div class="scout-trails-help-page">
   <style type="text/css">
-    .scout-help-page ol li { list-style : decimal inside; margin-left: 10px;}
+    .scout-trails-help-page ol li { list-style : decimal inside; margin-left: 10px;}
   </style>
-  <h1 style="font-size: 16px;">Scout help index</h1>
+  <h1 style="font-size: 16px;">Scout Trails help index</h1>
   <br />
 
   <div class="row">
@@ -10,9 +10,12 @@
       <h2 style="font-size: 14px;">Contents of this document</h2>
       <br />
       <ol>
-        <li>Install Joyride jQuery plugin.</li>
-        <li>Create Scout Tips and Scout Trails.</li>
-        <li>Create Joyride Tips content.</li>
+        <li>Explore Scout Tips & Trails.</li>
+        <li>Create a Scout Tip.</li>
+        <li>Add your Scout Tip to a Scout Trail.</li>
+        <li>Add your Scout Trail to a page, using Context.</li>
+        <li>If you don't see your Scout Trail...</li>
+        <li>For more information about Joyride.</li>
       </ol>
       <hr>
     </div>
@@ -22,13 +25,55 @@
 
   <div class="row">
     <div class="twelve columns">
-      <h2 style="font-size: 14px;">1. Install Joyride jQuery plugin.</h2>
+      <h2 style="font-size: 14px;">1. Explore Scout Tips & Trails.</h2>
+      <p>This module/features package includes some exported scout trails and context that will give you some idea of how to create Scout Tips & Trails, and how they behave.</p>
+      <p>If you have successfully installed scout and scout_trails (and all the dependencies), there should be some trails available in content administration.</p>
       <br />
       <ol>
-        <li>Download Joyride plugin from <a href="https://github.com/zurb/joyride" target="_blank">GitHub</a> repository. </li>
-        <li>Extract contents of the archive to <code>sites/all/lbraries/joyride</code> folder.</li>
-        <li>Check your site's <a href="/admin/reports/status" target="_blank">Status Report</a>. </li>
-        <li>You should see <code>Zurb Joyride jQuery plugin (required by Scout) | Installed ( jquery.joyride-2.0.3.js )</code></li>
+        <li>Navigate to <a href="http:/admin/content" target="_blank">admin/content</a> and you should see a tab labeled "Scout Trails." </li>
+        <li>Click that tab to go to <a href="http:/admin/content/scout-trails" target="_blank">/admin/content/scout-trails</a>.</li>
+        <li>There is a trail called "Scout Tips" (don't click).  It includes 7 tips.</li>
+        <li>Click on the first tip, <a href="http:/admin/content/scout-trails/scout-tips/1/edit" target="_blank">Scout Tips - Step One</a>.</li>
+        <li>There should be a block called "Scout Trail" somewhere in the content region (results vary by admin theme). This is the Scout Trailhead. Click the "Scout" link to start the trail tour.</li>
+      </ol>
+      <p><stong>Note 1:</strong> Scout Tips should point very closely to the content they are anchored to, but results can vary depending on themes and admin toolbars used.  These tips on admin pages were built for the Seven theme and Drupal's core admin menu + shortcuts.  For use with other themes, you may need to make style adjustments to css.  The default css is included in the Joyride library files.</p>
+      <p><stong>Note 2:</strong>These examples of Scout Tips in the scout tip creation forms include tips anchored to selectors inside field-sets.  This is generally not a great idea, because the tip cannot be properly anchored unless the fieldset is opened.  If you need to do this, you might consider adding tips instructing the user to open a fieldset before proceeding to the next tip.  I just don't follow my own advice.</p>
+
+      <hr>
+    </div>
+  </div>
+
+  <br />
+
+  <div class="row">
+    <div class="twelve columns">
+      <h2 style="font-size: 14px;">2. Create a Scout Tip.</h2>
+      <br />
+      <ol>
+        <li>Navigate to the <a href="/admin/content/scout-trails/scout-tips" target="_blank">Scout Tips</a> admin page.</li>
+        <li>Click the "add scout tip" link and complete the form.</li>
+        <li>Add text for the label (title), the body of the tip, and the "Next" button (or leave the default).</li>
+        <li>Location Attributes control how your tip is anchored to a page.  You can use a class or data selector, or make the tip modal.</li>
+        <li>Options includes only a custom style class, if you want to add one for your own style changes to the tips themselves.</li>
+        <li>Save your Scout Tip.</li>
+      </ol>
+      <p><stong>Note: </strong>Selectors on a given page must be unique or the tip will be anchored to the first occurance found on that page.</p>
+      <hr>
+    </div>
+  </div>
+
+  <br />
+
+  <div class="row">
+    <div class="twelve columns">
+      <h2 style="font-size: 14px;">3. Add your Scout Tip to a Scout Trail.</h2>
+      <br />
+      <ol>
+        <li>Navigate to the <a href="/admin/content/scout-trails" target="_blank">Scout Trails</a> admin page.</li>
+        <li>Click the "add scout trail" link and complete the form.</li>
+        <li>Add text for the label (trail name) and a brief description.</li>
+        <li>Select the tip you create by typing its name in the autocomplete field.  If you know its id, you can type that in parens. Like, (23).</li>
+        <li>Save your Scout Trail.</li>
       </ol>
       <hr>
     </div>
@@ -38,13 +83,17 @@
 
   <div class="row">
     <div class="twelve columns">
-      <h2 style="font-size: 14px;">2. Create context to manage your tour.</h2>
+      <h2 style="font-size: 14px;">4. Add your Scout Trail to a page, using Context.</h2>
       <br />
       <ol>
-        <li>Navigate to context <a href="/admin/structure/context" target="_blank">overview</a> page.</li>
-        <li>Optionally review module's bundled contexts to get an idea of how they works. Bundler contexts are tagged with "joyride" tag.</li>
-        <li>Create new context as you usually do. In "Conditions" section add "Path" condition and enter URLs of pages where you want to have your Joyride tour enabled.</li>
-        <li>In "Reactions" section paste tips content that will be shown as Joyride tour tips. <a href="#create-tips-content">More info on this.</a></li>
+        <li>Navigate to <a href="/admin/structure/context" target="_blank">Context</a> and click "Add."</li>
+        <li>Give your context a unique machine name.  It's also helpful to tag them and give them descriptions.</li>
+        <li>Add a condition. For the sake of example, it's easiest to use a path condition and add the path to your page.</li>
+        <li>Under Reactions, select "Scout Trail".</li>
+        <li>Use the pulldown list to find your trail.  It will tell you the name and how many tips are on it.</li>
+        <li>If you want the trail to start automatically, check the appropriate box.</li>
+        <li>If not, you must also add the trailhead block to a visible region on the page, so it can be started with a click.</li>
+        <li>Save your context.</li>
       </ol>
       <hr>
     </div>
@@ -54,19 +103,29 @@
 
   <div class="row">
     <div class="twelve columns">
-      <h2 id="create-tips-content" style="font-size: 14px;">3. Create Joyride Tips content.</h2>
+      <h2 style="font-size: 14px;">5. If you don't see your Scout Trail...</h2>
       <br />
       <ol>
-        <li>Joyride module expects that tips content will be formatted to use <code>&lt;li&gt;</code> as item wrappers</li>
-        <li>You can use following code as a reference:
-          <pre>
-  &lt;li data-id="step-welcome" data-button="Next" data-options="tipAnimation:fade"&gt;
-    &lt;h5>Tip heading&lt;/h5&gt;
-    &lt;p>Pellentesque ornare sem lacinia quam venenatis vestibulum.&lt;/p&gt;
-  &lt;/li&gt;
-          </pre>
-        </li>
-        <li>Please check <a href="http://www.zurb.com/playground/jquery-joyride-feature-tour-plugin" target="_blank">documentation</a> to see complete list of options and features.</li>
+        <li>... make sure you have <a href="/admin/people/permissions#module-scout_trails" target="_blank">permissions</a> to view trails.</li>
+        <li>... try starting it automatically if you did not already use that option.</li>
+        <li>... double-check the path you used in your context.</li>
+        <li>... make sure your tip was included in the trail you added to your context.</li>
+        <li>... make sure your tip is anchored to a unique class or data id on the page. Unanchored tips can appear, but float to the top of the page.</li>
+        <li>... try editing the tip and making it modal, so that it does not need to be anchored.</li>
+        <li>... review the help info for <a href="/admin/help/scout" target="_blank">Scout</a> and make sure your Joyride library is properly installed.</li>
+       </ol>
+      <hr>
+    </div>
+  </div>
+
+  <br />
+
+  <div class="row">
+    <div class="twelve columns">
+      <h2 id="create-tips-content" style="font-size: 14px;">6. For more information about Joyride.</h2>
+      <br />
+      <ol>
+        <li>If you would like to learn more about the configuration options for the Joyride plugin library, check out the <a href="http://www.zurb.com/playground/jquery-joyride-feature-tour-plugin" target="_blank">Zurb.Com documentation</a> to see a complete list of options and features.</li>
       </ol>
       <hr>
     </div>
